5cb50b5a3a83b9d41beb7c21794b9feb5034dbc6
Spencer Kittleson <spencer.kittleson@niceincontact.com>
2024-02-29 14:20:46 -0800

    sso and create user example

--- examples/server-tokens/python/create_user_when_not_found.py
+++ examples/server-tokens/python/create_user_when_not_found.py
@@ -2,13 +2,13 @@ import hashlib
 import time
 import requests
 import hmac
-import urllib.parse
 import xmltodict
 
 key = 'REDACTED'
 secret = 'REDACTED'
 user = '=foo'
 site = 'https://customer.mindtouch.com'
+
 
 epoch = str(int(time.time()))
 message_bytes = f'{key}_{epoch}_{user}'.encode('utf-8')
@@ -28,14 +28,8 @@ response = requests.get(
 response.raise_for_status()
 xml = xmltodict.parse(response.text)
 
-# this will create the user if it doesn't exist.
+# this will create the user if it doesn't exist. Only needs to be ran once
 if xml['user']['username'] == 'Anonymous':
     response = requests.get(
         f'{site}/@api/deki/users/authenticate?x-deki-token={token}', headers=headers, verify=False)
     response.raise_for_status()
-
-# Generate a link that a user can click that will authenticate and redirect to a file (only required once since the session has been created)
-redirect_encoded = urllib.parse.quote(
-    f"{site}/@api/deki/files/1/1ef17415582adac0a88cdf8171afc9c4.jpg", safe='')
-print(
-    f"User should click here now: {site}/@api/deki/users/authenticate?x-deki-token={token}&redirect={redirect_encoded}")
