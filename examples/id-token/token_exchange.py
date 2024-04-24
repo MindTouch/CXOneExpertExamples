552aee5ec1eb845cf9b22fa8d5ef69c3803eca8a
Spencer Kittleson <spencer.kittleson@niceincontact.com>
2024-04-24 09:02:55 -0700

    token exchange example

---
+++ examples/id-token/token_exchange.py
@@ -0,0 +1,29 @@
+import requests
+
+# an identity token jwt provided by the identity provider
+identity_token_jwt = "REPLACEME"
+
+# Expert site/domain
+domain = 'customer.mindtouch.com'
+
+# keep a session (for repeated requests as this user)
+session = requests.Session()
+
+# Exchange identity token for user session using a matching identity provider
+response = session.post(
+    f'https://{domain}/@app/auth/token/exchange', data=identity_token_jwt, headers={
+        'Content-Type': 'application/json'
+    })
+if response.status_code == 200:
+
+    # User has been provisioned based on the identity token.
+    user = response.json()
+    if user['@anonymous'] == 'false':
+        print('Welcome ' + user['fullname'])
+        
+        # Additional requests as this user is now possible.
+    else:
+        raise Exception('Token exchanged failed')
+else:
+    response.raise_for_status()
+    print(response.text)
